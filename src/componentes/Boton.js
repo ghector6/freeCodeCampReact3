e1604fc961c9742562a4d0ca2958425fee45fb34
Hector Garcia Rosales <ghectormus@gmail.com>
2023-12-24 18:04:18 -0600

    fix: typo

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -3,12 +3,12 @@ import React from 'react';
 function Boton({texto, esBotonDeClick, handleClick}){
   return(
     <button
-      className={esBotonDeclick ? "boton-click" : "boton-reiniciar"}
+      className={esBotonDeClick ? "boton-click" : "boton-reiniciar"}
       onClick={handleClick}
     >
       {texto}
     </button>
-  )
+  );
 }
 
 export default Boton;
